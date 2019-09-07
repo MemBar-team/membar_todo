4275ef2e71053d05a03ca277befdaf21aedabceb
yuta karimata <ignorantbeginner@gmail.com>
2019-09-07 17:07:36 +0900

    default

--- manager/models.py
+++ manager/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class MembarTodo(models.Model):
+class membar_todo(models.Model):
+    #・タスク名 name varchar
+    #・タスク詳細 contents varchar
+    #・実行フラグ done_flag varchar(1)
+    #・論理削除フラグ delete_at varchar
+    #・作成日時 create_at datetime
+    name      = models.CharField()
+    contents  = models.CharField()
+    done_flag = models.CharField(max_length=1)
+    delete_at = models.DateField()
+    create_at = models.DateField()
